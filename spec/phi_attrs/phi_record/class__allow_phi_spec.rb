d8f33d3bcafa89fbcaee3d427720ae6f0b99c8e6
Eric Greer <eric@apsis.io>
2019-01-11 11:28:58 -0500

    adding block checks for allow and disallow

--- spec/phi_attrs/phi_record/class__allow_phi_spec.rb
+++ spec/phi_attrs/phi_record/class__allow_phi_spec.rb
@@ -100,4 +100,24 @@ RSpec.describe 'class allow_phi' do
       expect { patient_with_detail.patient_detail.detail }.to raise_error(access_error)
     end
   end
+
+  context 'block checks' do
+    context 'allow_phi' do
+      it 'succeeds' do
+        expect { PatientInfo.allow_phi!('ok', 'ok') }.not_to raise_error
+      end
+      it 'raises ArgumentError with block' do
+        expect { PatientInfo.allow_phi!('ok', 'ok') {} }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'allow_phi!' do
+      it 'succeeds' do
+        expect { PatientInfo.allow_phi('ok', 'ok') {} }.not_to raise_error
+      end
+      it 'raises ArgumentError for allow_phi! without block' do
+        expect { PatientInfo.allow_phi('ok', 'ok') }.to raise_error(ArgumentError)
+      end
+    end
+  end
 end

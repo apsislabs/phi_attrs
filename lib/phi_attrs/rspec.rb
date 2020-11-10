5b689b0c78627cd9afa7a214e252b61834974af2
Chris Pfohl <christopher.pfohl@gmail.com>
2020-11-10 09:01:33 -0500

    Debugged in live code

--- lib/phi_attrs/rspec.rb
+++ lib/phi_attrs/rspec.rb
@@ -1,20 +1,20 @@
 require 'rspec/expectations'
 
-RSpec::Matchers.define :to_allow_phi_access do
-  match_for_should do |result|
-    @allowed = result.allow_phi?
+DO_NOT_SPECIFY = "do not specify `allowed_by` or `with_access_reason` for negated `allow_phi_access`"
+
+RSpec::Matchers.define :allow_phi_access do
+  match do |result|
+    @allowed = result.phi_allowed?
     @user_id_matches = @user_id.nil? || @user_id == result.phi_allowed_by
     @reason_matches = @reason.nil? || @reason == result.phi_access_reason
 
     @allowed && @user_id_matches && @reason_matches
   end
 
-  match_for_should_not do |result|
-    @disallowed = !result.allow_phi?
-    @user_id_does_not_match = @user_id.nil? || @user_id != result.phi_allowed_by
-    @reason_does_not_match = @reason.nil? || @reason != result.phi_access_reason
+  match_when_negated do |result|
+    raise ArgumentError, DO_NOT_SPECIFY unless @user_id.nil? && @reason.nil?
 
-    @disallowed && @user_id_does_not_match && @reason_does_not_match
+    !result.phi_allowed?
   end
 
   chain :allowed_by do |user_id|
@@ -27,33 +27,17 @@ RSpec::Matchers.define :to_allow_phi_access do
 
   # :nocov:
   failure_message do |result|
-    expected = ['Expected phi to be allowed']
-    expected << "by user: \"#{@user_id}\"" unless @user_id.nil?
-    expected << "for reason: \"#{@reason}\"" unless @reason.nil?
-    expected = "#{expected.join ' '}."
+    msgs = []
 
-    failure_reason = ['But']
-    failure_reason << 'it was not allowed' unless @allowed
-    failure_reason << "it was allowed by #{@result.phi_allowed_by}" unless @user_id_matches
-    failure_reason << "it was allowed because #{@result.phi_access_reason}" unless @reason_amtches
-    failure_reason = "#{failure_reason.join ', and '}."
+    msgs = ['PHI Access was not allowed.'] unless @allowed
+    msgs << "PHI Access was allowed by '#{result.phi_allowed_by}' (not '#{@user_id}')." unless @user_id_matches
+    msgs << "PHI Access was allowed because '#{result.phi_access_reason}' (not because '#{@reason}')." unless @reason_matches
 
-    [ expected, failure_reason ].join " "
+    msgs.join "\n"
   end
 
   failure_message_when_negated do |result|
-    expected = ['Did not expect phi to be allowed']
-    expected << "by user: \"#{@user_id}\"" unless @user_id.nil?
-    expected << "for reason: \"#{@reason}\"" unless @reason.nil?
-    expected = "#{expected.join ' '}."
-
-    failure_reason = ['But']
-    failure_reason << 'it was allowed' unless @disallowed
-    failure_reason << "it was allowed by #{@result.phi_allowed_by}" unless @user_id_does_not_match
-    failure_reason << "it was allowed because #{@result.phi_access_reason}" unless @reason_does_not_match
-    failure_reason = "#{failure_reason.join ', and '}."
-
-    [ expected, failure_reason ].join " "
+    "PHI access was allowed by '#{result.phi_allowed_by}', because '#{result.phi_access_reason}'"
   end
   # :nocov:
 end

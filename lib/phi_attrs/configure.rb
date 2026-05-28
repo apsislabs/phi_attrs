98a3669c1fdce6865a83b610c4cec11bfed88834
Wyatt Kirby <wyatt@apsis.io>
2026-05-28 14:29:09 -0400

    chore: replace class variables with mattr_accessor, scope FORMAT constant

--- lib/phi_attrs/configure.rb
+++ lib/phi_attrs/configure.rb
@@ -1,53 +1,13 @@
 # frozen_string_literal: true
 
 module PhiAttrs
-  @@log_path = nil
-  @@log_shift_age = 0 # Default to disabled
-  @@log_shift_size = 1_048_576 # 1MB - Default from logger class
-  @@current_user_method = nil
-  @@translation_prefix = "phi"
+  mattr_accessor :log_path, default: nil
+  mattr_accessor :log_shift_age, default: 0
+  mattr_accessor :log_shift_size, default: 1_048_576
+  mattr_accessor :current_user_method, default: nil
+  mattr_accessor :translation_prefix, default: "phi"
 
   def self.configure
     yield self if block_given?
-  end
-
-  def self.log_path
-    @@log_path
-  end
-
-  def self.log_path=(value)
-    @@log_path = value
-  end
-
-  def self.log_shift_age
-    @@log_shift_age
-  end
-
-  def self.log_shift_age=(value)
-    @@log_shift_age = value
-  end
-
-  def self.log_shift_size
-    @@log_shift_size
-  end
-
-  def self.log_shift_size=(value)
-    @@log_shift_size = value
-  end
-
-  def self.translation_prefix
-    @@translation_prefix
-  end
-
-  def self.translation_prefix=(value)
-    @@translation_prefix = value
-  end
-
-  def self.current_user_method
-    @@current_user_method
-  end
-
-  def self.current_user_method=(value)
-    @@current_user_method = value
   end
 end
--- lib/phi_attrs/formatter.rb
+++ lib/phi_attrs/formatter.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 module PhiAttrs
-  FORMAT = "%s %5s: %s\n"
-
   # https://github.com/ruby/ruby/blob/trunk/lib/logger.rb#L587
   class Formatter < ::Logger::Formatter
+    FORMAT = "%s %5s: %s\n"
+
     def call(severity, timestamp, _progname, msg)
       format(FORMAT, format_datetime(timestamp), severity, msg2str(msg))
     end

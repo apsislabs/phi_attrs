27717cd0fbd20fe187eb0970279a21b029abfe41
Wyatt Kirby <wyatt@apsis.io>
2026-06-05 16:51:55 -0400

    test: avoid constants in extend phi spec block

--- spec/phi_attrs/phi_record/extend_phi_access_spec.rb
+++ spec/phi_attrs/phi_record/extend_phi_access_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require "spec_helper"
 
-RSpec.describe 'extend_phi_access' do
+RSpec.describe "extend_phi_access" do
   before(:all) do
     ActiveRecord::Schema.define do
       create_table :extend_access_children, force: true do |t|
@@ -17,21 +17,21 @@ RSpec.describe 'extend_phi_access' do
       end
     end
 
-    class ExtendAccessChild < ApplicationRecord
-      self.table_name = 'extend_access_children'
+    Object.const_set(:ExtendAccessChild, Class.new(ApplicationRecord) do
+      self.table_name = "extend_access_children"
 
       phi_model
       exclude_from_phi :id, :created_at, :updated_at
 
       def serializable_hash(options = nil)
         super.tap do |hash|
-          hash['echo_first_name'] = first_name
+          hash["echo_first_name"] = first_name
         end
       end
-    end
+    end)
 
-    class ExtendAccessParent < ApplicationRecord
-      self.table_name = 'extend_access_parents'
+    Object.const_set(:ExtendAccessParent, Class.new(ApplicationRecord) do
+      self.table_name = "extend_access_parents"
       belongs_to :extend_access_child
 
       phi_model
@@ -41,12 +41,12 @@ RSpec.describe 'extend_phi_access' do
       def child_hash
         extend_access_child.serializable_hash
       end
-    end
+    end)
   end
 
-  it 'does not make implicit new-record access a durable extension' do
-    child = ExtendAccessChild.create!(first_name: 'Jane')
-    parent = ExtendAccessParent.new(name: 'parent', extend_access_child: child)
+  it "does not make implicit new-record access a durable extension" do
+    child = ExtendAccessChild.create!(first_name: "Jane")
+    parent = ExtendAccessParent.new(name: "parent", extend_access_child: child)
 
     # New records have implicit PHI access. Touching the relation before the
     # parent is saved should not grant or track access on the persisted child.
@@ -57,19 +57,19 @@ RSpec.describe 'extend_phi_access' do
     parent.save!
 
     expect do
-      parent.get_phi('user@example.com', 'serialize child') { parent.child_hash }
+      parent.get_phi("user@example.com", "serialize child") { parent.child_hash }
     end.not_to raise_error
   end
 
-  it 're-grants an extended relation when the tracked relation is stale' do
-    child = ExtendAccessChild.create!(first_name: 'Jane')
-    parent = ExtendAccessParent.create!(name: 'parent', extend_access_child: child)
+  it "re-grants an extended relation when the tracked relation is stale" do
+    child = ExtendAccessChild.create!(first_name: "Jane")
+    parent = ExtendAccessParent.create!(name: "parent", extend_access_child: child)
 
     parent.instance_variable_get(:@__phi_relations_extended).add(child)
     expect(child.phi_allowed?).to be(false)
 
     expect do
-      parent.get_phi('user@example.com', 'serialize child') { parent.child_hash }
+      parent.get_phi("user@example.com", "serialize child") { parent.child_hash }
     end.not_to raise_error
   end
 end
